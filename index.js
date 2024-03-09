9caffb33360baf2822bcd27923b88fbd809e9002
Farhan Malik <farhanmalik0709@gmail.com>
2024-03-09 21:09:13 +0530

    logo added

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const imgUrl = [
   "https://images.unsplash.com/photo-1579483885149-47cd52fcc7ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1602174940535-c9849f914093?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1627139873310-4562739973a6?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-6f0f229b-be2a-4b0a-b523-a0b3e5287cd2.jpg?w=109&e=webp",
+  "https://images.unsplash.com/photo-1590074247122-baef7a2e2b53?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1596940301147-e8b2d240e6e8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1579483885149-47cd52fcc7ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1602174940535-c9849f914093?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1627139873310-4562739973a6?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-6f0f229b-be2a-4b0a-b523-a0b3e5287cd2.jpg?w=109&e=webp",
   "https://images.unsplash.com/photo-1590074247122-baef7a2e2b53?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1596940301147-e8b2d240e6e8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1579483885149-47cd52fcc7ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -41,11 +48,15 @@ const imgUrl = [
   "https://images.unsplash.com/photo-1627139873310-4562739973a6?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1590074247122-baef7a2e2b53?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1596940301147-e8b2d240e6e8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1579483885149-47cd52fcc7ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1602174940535-c9849f914093?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1627139873310-4562739973a6?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1590074247122-baef7a2e2b53?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1596940301147-e8b2d240e6e8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-633191c0-c70e-4fe9-bbb1-098c97204fc7.jpg?w=109&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-64f3b57e-d65b-4804-b598-a81e9f55cc99.jpg?w=109&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-939cf1b9-3c9d-4979-b3c4-56784ef01bca.jpg?w=109&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-67c48f45-1fd0-4de8-9030-97c4f6684880.jpg?w=109&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-c11ad59a-5065-4913-bd0e-6d066c240f6d.jpg?w=112&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-b6cedaae-63e9-499c-8f72-aa0b136459cd.jpg?w=112&e=webp",
+  "https://d2kq0urxkarztv.cloudfront.net/52563c98fcc7636368000051/4301011/image-649f83c4-f65b-4ea4-8e96-f503670a91c4.jpg?w=109&e=webp",
+
+
 ];
 
 const imageGrid=document.getElementById('image-grid')
